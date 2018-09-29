5b2044caa593c87a6f9f00b8af5e5c876199676c
kodyclemens <kody.clemens@gmail.com>
2018-09-29 18:35:29 -0400

    Done

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
 def square_array(array)
-  # your code here
+  square_roots = []
+  array.each do |number_to_square|
+    square_roots.push(number_to_square*number_to_square)
+  end
+  square_roots
 end
