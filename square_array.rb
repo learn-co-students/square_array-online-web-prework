815757706cfd5b318e08537b30235892864545b5
Janita Madramootoo <jaymadr@gmail.com>
2018-10-30 23:44:54 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
-def square_array(array)
-  # your code here
-end
+
+def square_array(numbers)
+    new_numbers = []
+
+    numbers.each do |n|
+      new_numbers.push(n**2)
+    end
+    new_numbers
+  end
+
+=begin
+I want:
+for each number in numbers
+  square each number and
+  return the value into a new array
+=end
