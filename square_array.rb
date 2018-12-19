333441c2ce31b89d2694a86ea4bedf99502abf62
Luis Zabala <luis.zabala076@gmail.com>
2018-12-19 21:08:27 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,6 @@
 def square_array(array)
-  new_array = []
-  array.each { |element| new_array << element ** 2 }
-    new_array
+  numbers = []
+  array.each {|element| numbers << element ** 2}
+    numbers
 end
+
