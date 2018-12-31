84168a30fe0566829f256d4ecaca64168ec8b47a
Cillian Wing <cwing@zagmail.gonzaga.edu>
2018-12-31 01:03:17 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
   # your code here
+  new_array = []
+  array.each do |sub_array|
+    square_number = sub_array ** 2
+    new_array << square_number
+  end
+  new_array
 end
