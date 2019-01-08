c22450c30b85f183725d6f9a2e5fc0cbd5708938
LaRaye Johnson <lnj2105@gmail.com>
2019-01-08 03:37:43 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -3,3 +3,11 @@ def square_array(numbers)
   numbers.each {|x| new_numbers << x ** 2}
   new_numbers
 end
+
+def square_array_with_collect(numbers)
+  squared_elements = numbers.collect {|x| x ** 2}
+end
+
+def another_square_array_with_collect(numbers)
+  squared_elements = numbers.collect {|x| x ** 2}
+end
