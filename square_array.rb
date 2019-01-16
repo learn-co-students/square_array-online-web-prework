2c8920a155b441ea437a99019394e5d95f2f5cd2
Vinh Do <vinhdo07@gmail.com>
2019-01-16 02:50:41 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
+require 'pry'
+# def square_array(array)
+#   counter = 0
+#   new_array = []
+#   while counter < array.size
+#     new_square = array.each**2
+#     new_array.push(new_square)
+#     counter += 1
+#   end
+#   return new_array
+# end
+
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each {|x| new_array << x*x}
+  return new_array
 end
