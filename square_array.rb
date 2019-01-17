3954ce63c5113aa31805659a859e827f3ac8d696
Lu Daley <daleyluis@gmail.com>
2019-01-17 20:50:52 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,20 @@
-def square_array(array)
-  # your code here
+#=begin
+def square_array(numbers)
+new_numbers = []  
+  #square them then add to new numbers array
+  numbers.each {|n| new_numbers << n**2}
+  new_numbers
 end
+
+# your code here
+#end
+#=end
+
+=begin
+def square_array(numbers)
+  Enumerator.new do |y|
+    numbers.each { |e| y << e ** 2 }    
+  end
+  .take(numbers.length)
+end
+=end
