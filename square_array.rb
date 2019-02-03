e244673c002843a63ace77b8e3d3f600fde21d12
jolly-hardware-0578 <lmccoy8911@gmail.com>
2019-02-03 18:49:46 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
+require 'pry'
 def square_array(array)
-  # your code here
+   new_array = []
+  array.each do |number|
+  #  binding.pry
+  squared = number * number  
+  new_array << squared
+end
+ return new_array
 end
