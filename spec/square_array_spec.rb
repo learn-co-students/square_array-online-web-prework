4304b1633670934c9659026d13311c66be95a4e7
Angelina Montanez <angelinamontanez80@gmail.com>
2019-02-03 20:40:18 +0000

    Done.

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -2,6 +2,7 @@ describe '#square_array' do
 
   it 'does not call on collect/map or inject' do
     numbers = [1,2,3]
+
     expect(numbers).to_not receive(:collect)
     expect(numbers).to_not receive(:map)
     expect(numbers).to_not receive(:inject)
@@ -11,11 +12,11 @@ describe '#square_array' do
   it 'calls on each' do
     numbers = [1,2,3]
     expect(numbers).to receive(:each)
-    square_array(numbers)
+    square_array(2)
   end
 
   it 'should square the elements in an array' do
-    expect(square_array([1,2,3])).to eq([1,4,9])
+  .each_char { |chr|  }  expect(square_array([1,2,3])).to eq([1,4,9])
     expect(square_array([9,10,16,25])).to eq([81,100,256,625])
   end
 
