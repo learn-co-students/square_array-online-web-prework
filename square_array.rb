6fd79fdd4b5883fef9872df1785a8c80b292e963
Ryan Pethel <arpethel@gmail.com>
2019-02-16 22:11:34 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
+# def square_array(array)
+#   i = 0
+#   my_arr = []
+#   array.each do |ele|
+#     new_ele = (ele * ele)
+#     my_arr << new_ele
+#     i += 1
+#   end
+#   return my_arr
+# end
+
+
 def square_array(array)
-  # your code here
+  array.map! { |ele| ele ** 2 }
 end
