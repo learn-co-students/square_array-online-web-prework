2774596d84c287e7f2906b5b29cb3fa988482cdb
Darren L <darren.lou@gmail.com>
2019-02-19 01:17:04 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
   # your code here
+  
+  array_item = 0
+  
+  array.each do
+    array[array_item] = array[array_item]**2
+    array_item += 1
+  end
+  return array
+  
+  # array.map {|x| x*x }
+  # return array
 end
