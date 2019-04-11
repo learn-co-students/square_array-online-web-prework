742e2d88e9a90e5726337e9e42662249ff92a6d9
Garth Lilly <garff247@gmail.com>
2019-04-11 06:40:40 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
+
 def square_array(array)
   # your code here
+  new_array = Array.new(array.size)
+  i = 0
+  array.each do |number|
+    new_array[i] = number * number
+    i += 1
+
+  end
+  return new_array
 end
