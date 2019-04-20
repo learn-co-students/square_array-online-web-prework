8e510247150d324fba2ac96927efa0bb8dc35898
Michael Chen <michaelchengaming@gmail.com>
2019-04-20 15:03:58 -0700

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
-  # your code here
-  squared_num = []
-  array.each {|num| squared_num << num**2}
-  return squared_num
+  squared = []
+  array.each { |numbers| squared << (numbers**2) }
+  return squared
+
+# array.map { |numbers| numbers**2} ##collect/map##
 end
