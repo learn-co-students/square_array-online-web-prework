2ea759fbb7dd68bb0f60efc5ebcc3bc979676272
Khristian Punzalan <khrispunzalan@gmail.com>
2019-05-20 20:42:12 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  numbers = [1, 2, 3]
+  numbers.each do
+  
+  square_array(numbers)
+  numbers.sqrt ("#{numbers}")
+  
+  square_array(new_numbers)
+  new_numbers.sqrt ("{new_numbers}")
+  
 end
