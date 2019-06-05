2b4dbad2edb51dea0bb1bff872e1055f9b70ff25
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-05 08:25:00 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+ new_array = []
+ i = 0
+ array.each do |element|
+  new_element = element*element
+  new_array << new_element
+ end
+ return new_array
 end
+
+square_array([3,4,5])
